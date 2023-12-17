cd6fefdba864d478e16bc397baa4c949ec413718
Alex <calium_by@mail.ru>
2023-12-17 16:26:55 +0300

    feat: solve vigenere-cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,16 +20,77 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor (direct = true) {
+    this.direct = direct
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  encrypt(str, code) {
+    if (!str || !code) {
+      throw new Error('Incorrect arguments!');
+    } else {
+      let eStr = [];
+      let k = 0;
+      while (str.length > code.length) {
+        code += code;
+      }
+      for (let i = 0; i < str.length; i++) {
+        if (str[i].toUpperCase().charCodeAt(0) > 90 || str[i].toUpperCase().charCodeAt(0) < 65 ) {
+          eStr.push(str[i]);;
+          k--;
+        } else {
+          const shifts = code[k].toUpperCase().charCodeAt(0) - 65;
+          let position = str[i].toUpperCase().charCodeAt(0);
+          for (let j = 0; j < shifts; j++) {
+            position++;
+            if (position > 90) {
+              position = 65;
+            }
+          }
+          eStr.push(String.fromCharCode(position))
+        }
+        k++;
+      }
+      if (!this.direct) {
+        eStr.reverse();
+      }
+      return eStr.join('');
+    }
+   
+  }
+  decrypt(str, code) {
+    if (!str || !code) {
+      throw new Error('Incorrect arguments!');
+    } else {
+      let eStr = [];
+      let k = 0;
+      while (str.length > code.length) {
+        code += code;
+      }
+      for (let i = 0; i < str.length; i++) {
+        if (str[i].toUpperCase().charCodeAt(0) > 90 || str[i].toUpperCase().charCodeAt(0) < 65 ) {
+          eStr.push(str[i]);;
+          k--;
+        } else {
+          const shifts = code[k].toUpperCase().charCodeAt(0) - 65;
+          let position = str[i].toUpperCase().charCodeAt(0);
+          for (let j = 0; j < shifts; j++) {
+            position--;
+            if (position < 65) {
+              position = 90;
+            }
+          }
+          eStr.push(String.fromCharCode(position))
+        }
+        k++;
+      }
+      if (!this.direct) {
+        eStr.reverse();
+      }
+      return eStr.join('');
+    }
   }
 }
 
 module.exports = {
   VigenereCipheringMachine
 };
+
